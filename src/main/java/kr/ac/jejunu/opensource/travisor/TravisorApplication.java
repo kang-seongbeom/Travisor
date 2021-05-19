977498b42b0a6eb75618fe371cc6b51a53c5ecd6
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2021-05-19 16:13:03 +0900

    프로젝트 다시 생성

---
+++ src/main/java/kr/ac/jejunu/opensource/travisor/TravisorApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.opensource.travisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravisorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/jejunu/opensource/travisor/TravisorApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.jejunu.opensource.travisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
